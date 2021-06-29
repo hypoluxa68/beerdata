fa30af0949024f07499c9b13eb36e3d7d63790a9
hypoluxa68 <35181124+hypoluxa68@users.noreply.github.com>
2021-06-28 20:40:06 -0500

    working navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 	// copyright
 	$('#copy-footer').html('<p>&copy;' + new Date().getFullYear() + ' | me.</p>');
+	
+	// navigation
+	// open
+	$('#open-nav-bar i').click(function(){
+		$('#main-nav').css('width', '35%');
+		$('#site').css('margin-left', '35%');
+	});	
+	
+	// close
+	$('#close-nav-bar i').click(function(){
+		$('#main-nav').css('width', '0');
+		$('#site').css('margin-left', '0');
+	});
 });
