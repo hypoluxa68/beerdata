0ada7253ed336413a770a6055eaa5365569976b6
hypoluxa68 <35181124+hypoluxa68@users.noreply.github.com>
2021-07-01 14:41:38 -0500

    working

--- public/js/sheets.js
+++ public/js/sheets.js
@@ -15,8 +15,8 @@ $(document).ready(function(){
 			}			
 			beerData.push(formattedRow)
 			}
-			const byBeerData = beerData.sort((a,b) => (a.beername>b.beername) ? 1: -1);
-			function showBeers(){			
+			var beerDataBeer = []
+			function showBeers(){
 				for(var i = 0; i < beerData.length; i++){
 					var beerText = ''
 					beerText += '<div class="card">'
@@ -30,9 +30,35 @@ $(document).ready(function(){
 				}
 			}
 			
-			
-		showBeers();
+		$('#by-beer').click(function(){
+			for(var i = 0; i < beerData.sort((a,b) => (a.beername > b.beername) ? 1: -1).length; i++){
+				var beerText = ''
+				beerText += '<div class="card">'
+				beerText += '<div class="card-body">'
+				beerText += '<h3 class="card-title">' + beerData[i].beername + '</h3>'
+				beerText += '<div class="card-text">'
+				beerText += '<h5 class="' + beerData[i].breweryid + '">' + beerData[i].breweryname + '</h5>'
+				beerText += '<p><strong>Style:</strong><br/>' + beerData[i].style + '</p>'
+				beerText += '<p><strong>ABV:</strong><br/>' + beerData[i].abv + '</p>'
+				$('#beers').append(beerText)
+			}
+		})	
+		
+		$('#by-brewery').click(function(){
+			for(var i = 0; i < beerDataBrewery.length; i++){
+				var beerText = ''
+				beerText += '<div class="card">'
+				beerText += '<div class="card-body">'
+				beerText += '<h3 class="card-title">' + beerData[i].beername + '</h3>'
+				beerText += '<div class="card-text">'
+				beerText += '<h5 class="' + beerData[i].breweryid + '">' + beerData[i].breweryname + '</h5>'
+				beerText += '<p><strong>Style:</strong><br/>' + beerData[i].style + '</p>'
+				beerText += '<p><strong>ABV:</strong><br/>' + beerData[i].abv + '</p>'
+				$('#beers').append(beerText)
+			}
+		})
+		showBeers()
 		console.log(beerData)
-		console.log(byBeerData)
+		console.log(beerData.sort((a,b) => (a.beername > b.beername) ? 1: -1))
 	});
 });
